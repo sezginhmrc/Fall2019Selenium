db820287c7f2137bd5fcdae826e3ce67c4aa851c
sezginhmrc <sezginhamurcu@icloud.com>
2020-03-01 12:38:57 -0500

    first commit

---
+++ src/test/java/com/automation/tests/day1/MyFirstSeleniumScript.java
@@ -0,0 +1,21 @@
+package com.automation.tests.day1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class MyFirstSeleniumScript
+{
+    public static void main(String[] args) {
+       // setup chromeDriver
+        WebDriverManager.chromedriver().setup();
+        //create chromeDriver object
+
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("http://google.com");
+
+
+    }
+}
